bae33d7b08027d616cd86623e972f1d400c49e2e
Bayram EKER <eker600@gmail.com>
2024-05-19 00:47:51 +0300

    first commit

---
+++ src/main/java/com/example/ai/App.java
@@ -0,0 +1,8 @@
+package com.example.ai;
+
+public class App {
+    public static void main(String[] args) {
+        System.out.println("AI Project Started");
+        // Proje başlangıç kodları buraya
+    }
+}
